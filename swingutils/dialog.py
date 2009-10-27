d8b824030f4fc9619a876ac47b082c5c485e4aa6
agronholm <devnull@localhost>
2009-10-27 23:34:33 +0200

    Added dialog utility functions

---
+++ swingutils/dialog.py
@@ -0,0 +1,21 @@
+"""
+Contains convenient shortcuts to JOptionPane methods that show different kinds
+of dialogs.
+
+"""
+from javax.swing import JOptionPane
+
+
+def showErrorDialog(error, title=u'Error', parent=None):
+    JOptionPane.showMessageDialog(parent, error, title,
+                                  JOptionPane.ERROR_MESSAGE)
+
+
+def showWarningDialog(message, title=u'Warning', parent=None):
+    JOptionPane.showMessageDialog(parent, message, title,
+                                  JOptionPane.WARNING_MESSAGE)
+
+
+def showMessageDialog(message, title, parent=None):
+    JOptionPane.showMessageDialog(parent, message, title,
+                                  JOptionPane.INFORMATION_MESSAGE)
