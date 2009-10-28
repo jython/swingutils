30d1975c529543f649ba7e6871a9578c8830db8c
agronholm <devnull@localhost>
2009-10-29 00:03:54 +0200

    Added a proper project description

--- setup.py
+++ setup.py
@@ -14,7 +14,22 @@ setup(
     version='0.1',
     description="Makes using Java's Swing UI toolkit easy on Jython",
     long_description="""\
+A collection of utility classes and helper functions to make it easier to build
+Swing user interfaces with Jython. The helpers provide "pythonic" alternatives
+to often clumsy Java interfaces.
 
+Included in this package:
+ * enhanced table and list models
+ * JavaBean support
+ * enhanced file chooser
+ * shortcuts for common dialogs
+ * wrappers for loading `JFormDesigner <http://www.jformdesigner.com/>`_ forms
+ * decorators and functions for invoking Swing code from other threads
+ 
+More will follow as the project progresses.
+
+There are no releases yet, but you can check out the code from
+`BitBucket <http://bitbucket.org/agronholm/jython-swingutils/>`_.
 """,
     author='Alex Gronholm',
     author_email='swingutils@nextday.fi',
