18e0567f06ba1927042c96800c0e5da3865c87d9
agronholm <devnull@localhost>
2009-11-10 00:06:11 +0200

    Added ValueHolder

--- swingutils/binding.py
+++ swingutils/binding.py
@@ -1,9 +1,74 @@
 """This module lets you synchronize properties between two objects."""
+from swingutils.events import addPropertyListener
 import weakref
 
 from java.beans import PropertyChangeListener
 
-__all__ = ('connect',)
+from swingutils.beans import JavaBeanSupport
+
+__all__ = ('ValueHolder', 'BeanHolder', 'PropertyAdapter', 'connect')
+
+
+class ValueHolder(JavaBeanSupport):
+    """
+    Holds a single value and fires a property change event every time
+    that value is changed. Also mirrors the properties of the held object,
+    firing property change events when it receives such events from that
+    object.
+
+    """
+    _value = None
+    __wrapper = None
+
+    def __init__(self, initial=None):
+        self.value = initial
+
+    def __getattr__(self, name):
+        return getattr(self._value, name)
+
+    def __setattr__(self, name, value):
+        if name in dir(self):
+            object.__setattr__(self, name, value)
+        else:
+            setattr(self._value, name, value)
+
+    def _getvalue(self):
+        return self._value
+
+    def __delegatePropertyChange(self, event):
+        self.firePropertyChange(event.propertyName, event.oldValue,
+                                event.newValue)
+
+    def _setvalue(self, newValue):
+        oldValue = self._value
+        if oldValue and self.__wrapper:
+            oldValue.removePropertyChangeListener(self.__wrapper)
+
+        self._value = newValue
+        self.firePropertyChange('value', oldValue, newValue)
+
+        if hasattr(newValue, 'addPropertyChangeListener'):
+            self.__wrapper = addPropertyListener(newValue, None,
+                                                 self.__delegatePropertyChange)
+
+        # Notify listeners of changes in all properties, including properties
+        # from both the old and new values
+        oldProps = set([p for p in dir(oldValue) if not p.startswith('_')])
+        newProps = set([p for p in dir(newValue) if not p.startswith('_')])
+        properties = oldProps.union(newProps)
+        properties.discard('value')
+        for property in properties:
+            oldVal = getattr(oldValue, property, None)
+            newVal = getattr(newValue, property, None)
+            self.firePropertyChange(property, oldVal, newVal)
+
+    value = property(_getvalue, _setvalue)
+
+    def addValueListener(self, listener, *args, **kwargs):
+        addPropertyListener(self, 'value', listener, *args, **kwargs)
+
+    def removeValueListener(self, listener):
+        self.removePropertyChangeListener('value', listener)
 
 
 class PropertyAdapter(PropertyChangeListener):
@@ -33,8 +98,8 @@ class PropertyAdapter(PropertyChangeListener):
             dst.removePropertyChangeListener(self)
 
 
-def connect(source, srcProperty, destination, dstProperty, twoway=False,
-            syncNow=False, converter=None):
+def connectProperties(source, srcProperty, destination, dstProperty,
+                      twoway=False, syncNow=False, converter=None):
     """
     Connects a property in the source object to a property in the destination
     object. When the source property changes, the destination property is set
--- tests/test_binding.py
+++ tests/test_binding.py
@@ -1,10 +1,117 @@
-from nose.tools import eq_, raises
+from nose.tools import eq_, raises, set_trace
 
 from swingutils.beans import AutoChangeNotifier
-from swingutils.binding import connect
+from swingutils.binding import ValueHolder, connectProperties
+from swingutils.events import addPropertyListener
 
 
-class TestBase(object):
+class TestValueHolder(object):
+    def setup(self):
+        self.holder = ValueHolder()
+        self.bean1 = AutoChangeNotifier()
+        self.bean2 = AutoChangeNotifier()
+        self.bean3 = AutoChangeNotifier()
+        self.bean1.prop1 = 'testVal1'
+        self.bean2.prop1 = 'testVal2'
+        self.bean3.prop2 = 'testVal3'
+
+    def testSetHolder(self):
+        self.holder.value = self.bean1
+        eq_(self.holder.prop1, 'testVal1')
+
+        self.holder.value = self.bean2
+        eq_(self.holder.prop1, 'testVal2')
+
+    def testValueChange(self):
+        def listener(event, listenerResult):
+            listenerResult[0] = event
+
+        listenerResult = [None]
+        self.holder.addValueListener(listener, listenerResult)
+
+        self.holder.value = self.bean1
+        event = listenerResult[0]
+        eq_(event.propertyName, 'value')
+        eq_(event.oldValue, None)
+        eq_(event.newValue, self.bean1)
+
+        self.holder.value = self.bean2
+        event = listenerResult[0]
+        eq_(event.propertyName, 'value')
+        eq_(event.oldValue, self.bean1)
+        eq_(event.newValue, self.bean2)
+
+    def testBeanSwitch(self):
+        self.holder.value = self.bean1
+        eq_(self.holder.value, self.bean1)
+        eq_(self.holder.prop1, 'testVal1')
+
+        self.holder.value = self.bean2
+        eq_(self.holder.value, self.bean2)
+        eq_(self.holder.prop1, 'testVal2')
+
+    def testBeanChangeEvents(self):
+        def listener(event, listenerResult):
+            listenerResult[0] = event
+
+        listenerResult1 = [None]
+        listenerResult2 = [None]
+        addPropertyListener(self.holder, 'prop1', listener, listenerResult1)
+        addPropertyListener(self.holder, 'prop2', listener, listenerResult2)
+
+        self.holder.value = self.bean1
+        event = listenerResult1[0]
+        eq_(event.oldValue, None)
+        eq_(event.newValue, 'testVal1')
+
+        self.holder.value = self.bean3
+        event1 = listenerResult1[0]
+        event2 = listenerResult2[0]
+        eq_(event1.oldValue, 'testVal1')
+        eq_(event1.newValue, None)
+        eq_(event2.oldValue, None)
+        eq_(event2.newValue, 'testVal3')
+
+    def testPropertyChange1(self):
+        """Test that passing a property change to the underlying value works"""
+        def listener(event, listenerResult):
+            listenerResult[0] = event
+
+        listenerResult = [None]
+        print "hasprop1=%s" % hasattr(self.holder, 'prop1')
+        self.holder.value = self.bean1
+        addPropertyListener(self.bean1, 'prop1', listener, listenerResult)
+
+        print "hasprop1=%s" % hasattr(self.holder, 'prop1')
+        self.holder.prop1 = 'anotherValue'
+        event = listenerResult[0]
+        eq_(event.propertyName, 'prop1')
+        eq_(event.oldValue, 'testVal1')
+        eq_(event.newValue, 'anotherValue')
+        eq_(self.bean1.prop1, 'anotherValue')
+
+    def testPropertyChange2(self):
+        """
+        Test that changing a property on the underlying value causes a
+        property change event to be fired on the holder.
+
+        """
+        def listener(event, listenerResult):
+            listenerResult[0] = event
+
+        listenerResult = [None]
+        self.holder.value = self.bean1
+        addPropertyListener(self.holder, 'prop1', listener, listenerResult)
+
+        self.bean1.prop1 = 'anotherValue'
+        event = listenerResult[0]
+        eq_(event.propertyName, 'prop1')
+        eq_(event.oldValue, 'testVal1')
+        eq_(event.newValue, 'anotherValue')
+        eq_(self.holder.prop1, 'anotherValue')
+
+
+class TestConnect(object):
     def setup(self):
         self.bean1 = AutoChangeNotifier()
         self.bean2 = AutoChangeNotifier()
@@ -12,7 +119,7 @@ class TestBase(object):
         self.bean2.prop1 = 'testVal2'
 
     def testOneWayNoSync(self):
-        connect(self.bean1, 'prop1', self.bean2, 'prop1')
+        connectProperties(self.bean1, 'prop1', self.bean2, 'prop1')
 
         # Nothing should've changed yet
         eq_(self.bean1.prop1, 'testVal1')
@@ -24,7 +131,8 @@ class TestBase(object):
         eq_(self.bean2.prop1, 'testVal3')
 
     def testOneWaySync(self):
-        connect(self.bean1, 'prop1', self.bean2, 'prop1', syncNow=True)
+        connectProperties(self.bean1, 'prop1', self.bean2, 'prop1',
+                          syncNow=True)
 
         # Bean2.prop1 should have changed
         eq_(self.bean1.prop1, 'testVal1')
@@ -36,7 +144,7 @@ class TestBase(object):
         eq_(self.bean2.prop1, 'testVal3')
 
     def testTwoWayNoSync(self):
-        connect(self.bean1, 'prop1', self.bean2, 'prop1', True)
+        connectProperties(self.bean1, 'prop1', self.bean2, 'prop1', True)
 
         # Nothing should've changed yet
         eq_(self.bean1.prop1, 'testVal1')
@@ -53,7 +161,7 @@ class TestBase(object):
         eq_(self.bean2.prop1, 'testVal4')
 
     def testTwoWaySync(self):
-        connect(self.bean1, 'prop1', self.bean2, 'prop1', True, True)
+        connectProperties(self.bean1, 'prop1', self.bean2, 'prop1', True, True)
 
         # Bean2.prop1 should have changed
         eq_(self.bean1.prop1, 'testVal1')
@@ -70,8 +178,8 @@ class TestBase(object):
         eq_(self.bean2.prop1, 'testVal4')
 
     def testConverter(self):
-        connect(self.bean1, 'prop1', self.bean2, 'prop1',
-                converter=lambda v: 'xx%sxx' % v)
+        connectProperties(self.bean1, 'prop1', self.bean2, 'prop1',
+                          converter=lambda v: 'xx%sxx' % v)
 
         # Nothing should've changed yet
         eq_(self.bean1.prop1, 'testVal1')
@@ -84,11 +192,11 @@ class TestBase(object):
 
     @raises(AssertionError)
     def testTwowayConverter(self):
-        connect(self.bean1, 'prop1', self.bean2, 'prop1', True,
-                converter=lambda v: 'xx%sxx' % v)
+        connectProperties(self.bean1, 'prop1', self.bean2, 'prop1', True,
+                          converter=lambda v: 'xx%sxx' % v)
 
     def testDisconnect(self):
-        adapter = connect(self.bean1, 'prop1', self.bean2, 'prop1')
+        adapter = connectProperties(self.bean1, 'prop1', self.bean2, 'prop1')
 
         # Nothing should've changed yet
         eq_(self.bean1.prop1, 'testVal1')
