aa8f7ed7267e4360b8e8ff57ea1f2371eb23442f
agronholm <devnull@localhost>
2010-02-24 22:26:46 +0200

    Refactored adapter framework for better design

--- swingutils/binding/adapters.py
+++ swingutils/binding/adapters.py
@@ -3,123 +3,135 @@ from swingutils.events import addPropertyListener, addExplicitEventListener
 
 class AdapterRegistry(object):
     def __init__(self):
-        self._propertyAdapters = {}
-        self._listAdapters = {}
-    
-    def _registerAdapter(self, cls, adapters):
-        try:
-            classname = cls.__classname__
-        except AttributeError:
-            raise AttributeError(u'Adapter class %s is missing the '
-                                 u'__classname__ attribute' %
-                                 self._getClassName(cls))
+        self.propertyAdapters = {}
+        self.listAdapters = {}
 
-        adapters[classname] = cls
-        return cls
-    
+    def _getClassNames(self, cls, names=None, level=0):
+        """Retrieves the class name of `cls` and names of its superclasses."""
+
+        if names is None:
+            names = []
+        clsname = u'%s.%s' % (cls.__module__, cls.__name__)
+
+        # Skip proxy classes
+        if not u'$' in clsname:
+            names.append(clsname)
+        for basecls in cls.__bases__:
+            self._getClassNames(basecls, names, level + 1)
+        return names
+
     def registerPropertyAdapter(self, cls):
-        return self._registerAdapter(cls, self._propertyAdapters)
+        properties = cls.__targetproperty__
+        if isinstance(cls.__targetproperty__, basestring):
+            properties = (cls.__targetproperty__,)
+        
+        for property in properties:
+            key = (cls.__targetclass__, property)
+            self.propertyAdapters[key] = cls
+
+        return cls
 
     def registerListAdapter(self, cls):
-        return self._registerAdapter(cls, self._listAdapters)
+        key = cls.__targetclass__
+        self.listAdapters[key] = cls
+        return cls
 
-    @staticmethod
-    def _getClassName(cls):
-        if cls.__module__ not in ('__builtin__', '__main__'):
-            return u'%s.%s' % (cls.__module__, cls.__name__)
-        return cls.__name__
-
-    def _getAdapter(self, candidates, default, obj, options):
+    def getPropertyAdapter(self, obj, property, options):
         # Gather a list of class names from the inheritance chain
-        classnames = []
-        cls = obj.__class__
-        while cls:
-            classnames.append(self._getClassName(cls))
-            cls = getattr(cls, 'superclass', None)
+        targetClassNames = self._getClassNames(obj.__class__)
+        targetClassNames.sort()
 
         # Find the nearest matching adapter for this class
-        bestMatch = default
-        bestMatchIndex = None
-        for key, value in candidates.iteritems():
-            if key in classnames:
-                index = classnames.index(key)
-                if not bestMatchIndex or bestMatchIndex > index:
-                    bestMatch = value
-                    bestMatchIndex = index
-                if index == 0:
-                    break
+        adapterClass = DefaultPropertyAdapter
+        for className in targetClassNames:
+            key = (className, property)
+            if key in self.propertyAdapters:
+                adapterClass = self.propertyAdapters[key]
+                break
 
-        # Instantiate the adapter with the given options
-        return bestMatch(options) if bestMatch else None
-
-    def getPropertyAdapter(self, obj, options):
-        return self._getAdapter(self._propertyAdapters, BasePropertyAdapter,
-                                obj, options)
+        return adapterClass(property, options)
 
     def getListAdapter(self, obj, options):
-        return self._getAdapter(self._listAdapters, None, obj, options)
+        # Gather a list of class names from the inheritance chain
+        targetClassNames = self._getClassNames(obj.__class__)
+        targetClassNames.sort()
+
+        # Find the nearest matching adapter for this class
+        adapterClass = None
+        for className in targetClassNames:
+            if className in self.listAdapters:
+                adapterClass = self.listAdapters[className]
+                break
+
+        if adapterClass:
+            return adapterClass(options)
 
 registry = AdapterRegistry()
 
 
-class BasePropertyAdapter(object):
+class BaseAdapter(object):
     listener = None
-    
-    def __init__(self, options):
-        pass
 
-    def addListener(self, obj, property, callback, *args, **kwargs):
-        self.listener = addPropertyListener(obj, property, callback, *args,
-                                            **kwargs)
-
-    def removeListener(self):
+    def removeListeners(self):
         if self.listener is not None:
             self.listener.unlisten()
             del self.listener
 
 
-@registry.registerPropertyAdapter
-class AbstractButtonAdapter(BasePropertyAdapter):
-    __classname__ = 'javax.swing.AbstractButton'
+class DefaultPropertyAdapter(BaseAdapter):
+    def __init__(self, property, options):
+        self.property = property
 
-    def addListener(self, obj, property, callback, *args, **kwargs):
-        if property == 'selected':
-            from java.awt.event import ItemListener
-            self.itemlistener = addExplicitEventListener(obj, ItemListener,
-                'itemStateChanged', callback, *args, **kwargs)
-        else:
-            BasePropertyAdapter.addListener(self, obj, property, callback,
+    def addListeners(self, obj, callback, *args, **kwargs):
+        self.listener = addPropertyListener(obj, self.property, callback,
                                             *args, **kwargs)
 
 
 @registry.registerPropertyAdapter
-class JTextComponentAdapter(BasePropertyAdapter):
+class ItemSelectableAdapter(DefaultPropertyAdapter):
+    __targetclass__ = 'java.awt.ItemSelectable'
+    __targetproperty__ = 'selected'
+
+    def addListeners(self, obj, callback, *args, **kwargs):
+        from java.awt.event import ItemListener
+        self.listener = addExplicitEventListener(obj, ItemListener,
+            'itemStateChanged', callback, *args, **kwargs)
+
+
+@registry.registerPropertyAdapter
+class JTextComponentAdapter(DefaultPropertyAdapter):
     """
+    Adapter for text components like JTextField, JFormattedTextField and
+    JTextArea.
+    
     :ivar onFocusLost: ``True`` if the binding should be synchronized
         when the field loses focus, ``False`` to synchronize whenever the
         associated document is changed. Default is ``False``.
 
     """
-    __classname__ = 'javax.swing.text.JTextComponent'
+    __targetclass__ = 'javax.swing.text.JTextComponent'
+    __targetproperty__ = 'text'
 
-    def __init__(self, options):
+    def __init__(self, property, options):
         self.onFocusLost = options.get('onFocusLost', False)
         self.docListeners = []
 
-    def addListener(self, obj, property, callback, *args, **kwargs):
-        if property == 'text':
-            if self.onFocusLost:
-                self.addFocusListener(obj, callback, *args, **kwargs)
-            else:
-                # Track changes to both JTextComponent.document and the
-                # document itself
-                self.listener = addPropertyListener(obj, 'document',
-                    self.documentChanged, obj, callback, *args, **kwargs)
-                self.addDocumentListeners(obj.document, callback, *args,
-                                          **kwargs)
+    def addListeners(self, obj, callback, *args, **kwargs):
+        if self.onFocusLost:
+            self.addFocusListener(obj, callback, *args, **kwargs)
         else:
-            BasePropertyAdapter.addListener(self, obj, property, callback,
-                                            *args, **kwargs)
+            # Track changes to both JTextComponent.document and the
+            # document itself
+            self.listener = addPropertyListener(obj, 'document',
+                self.documentChanged, obj, callback, *args, **kwargs)
+            self.addDocumentListeners(obj.document, callback, *args,
+                                      **kwargs)
+
+    def removeListeners(self):
+        DefaultPropertyAdapter.removeListeners(self)
+        for listener in self.docListeners:
+            listener.unlisten()
+        del self.docListeners[:]
 
     def addFocusListener(self, obj, callback, *args, **kwargs):
         from java.awt.event import FocusListener
@@ -138,22 +150,22 @@ class JTextComponentAdapter(BasePropertyAdapter):
         self.addDocumentListeners(obj, event.newValue)
         callback(*args, **kwargs)
 
-    def removeListener(self):
-        BasePropertyAdapter.removeListener(self)
-        for listener in self.docListeners:
-            listener.unlisten()
-        del self.docListeners[:]
-
 
 @registry.registerPropertyAdapter
-class JListAdapter(BasePropertyAdapter):
+class JListAdapter(DefaultPropertyAdapter):
     """
+    Adapter for :class:`javax.swing.JList`.
+    
     :ivar ignoreAdjusting: ``True`` if the callback should only be called
         when the selection list has finished adjusting.
         Default is ``True``.
 
     """
-    __classname__ = 'javax.swing.JList'
-    
-    def __init__(self, options):
+    __targetclass__ = 'javax.swing.JList'
+    __targetproperty__ = ('selectedItem', 'selectedIndex',
+                          'leadSelectionIndex', 'anchorSelectionIndex',
+                          'maxSelectionIndex', 'minSelectionIndex')
+
+    def __init__(self, property, options):
+        DefaultPropertyAdapter.__init__(self, property, options)
         self.ignoreAdjusting = options.get('ignoreAdjusting', True)
