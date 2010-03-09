d7351cde48cd38c61313120ab2f648a73919cb02
agronholm <devnull@localhost>
2010-03-09 21:23:44 +0200

    Moved the contents of dispatch.py to threads.py

--- swingutils/threads.py
+++ swingutils/threads.py
@@ -5,19 +5,16 @@ from java.lang import Runnable, Throwable
 from java.util.concurrent import ThreadPoolExecutor, TimeUnit, \
     LinkedBlockingDeque, Future, ExecutionException, \
     CancellationException
+from javax.swing import SwingUtilities
 
 __all__ = ('TaskExecutor',)
 
 
 class RunnableWrapper(Runnable):
-    def __init__(self, func, args, kwargs, name=None, beforeCallback=None,
-                 afterCallback=None):
+    def __init__(self, func, args, kwargs):
         self._func = func
         self._args = args
         self._kwargs = kwargs
-        self.name = name
-        self.beforeCallback = beforeCallback
-        self.afterCallback = afterCallback
 
     def run(self):
         self._func(*self._args, **self._kwargs)
@@ -124,3 +121,66 @@ class TaskExecutor(ThreadPoolExecutor):
                              afterCallback)
         self.execute(result)
         return result
+
+
+def execInEDT(func, *args, **kwargs):
+    """
+    Run the given function in the Event Dispatch Thread.
+    The calling thread will block until the function has been run.
+    Any exceptions will be propagated to the calling thread.
+
+    """
+    if SwingUtilities.isEventDispatchThread():
+        return func(*args, **kwargs)
+    else:
+        holder = AsyncResult(func, args, kwargs)
+        SwingUtilities.invokeAndWait(holder)
+        return holder.get()
+
+
+def invokeInEDT(func):
+    """
+    Decorator that causes the wrapped function to be queued for execution
+    in the event dispatch thread. The calling thread will block
+    until the function has executed in the event dispatch thread.
+    If the target function is called from the event dispatch thread,
+    it will be executed directly The return value of the target function
+    is preserved and returned always.
+
+    """
+    def wrapper(*args, **kwargs):
+        return execInEDT(func, *args, **kwargs)
+    return wrapper
+
+
+def invokeLater(func):
+    """
+    Decorator that ensures that the given function is executed in the Event
+    Dispatch Thread. If the current thread is the EDT, the function is executed
+    normally. Otherwise, it is queued for execution in the EDT. The return
+    value and any raised exception are always discarded.
+
+    """
+    def wrapper(*args, **kwargs):
+        if SwingUtilities.isEventDispatchThread():
+            func(*args, **kwargs)
+        else:
+            wrapper = RunnableWrapper(func, args, kwargs)
+            SwingUtilities.invokeLater(wrapper)
+    return wrapper
+
+
+def asyncSwingTask(func):
+    """
+    Decorator that causes the wrapped function to be queued for execution
+    in the Event Dispatch Thread. The call will return immediately.
+
+    :return: a result holder that will contain the return value
+    :rtype: :class:`~AsyncResult`
+
+    """
+    def wrapper(*args, **kwargs):
+        holder = AsyncResult(func, args, kwargs)
+        SwingUtilities.invokeLater(holder)
+        return holder
+    return wrapper
