a84557eab8713c4354ae8334b9de6dcc0d7db5be
agronholm <devnull@localhost>
2010-03-10 21:38:30 +0200

    Added __all__, reverted the default messages change

--- swingutils/dialogs/basic.py
+++ swingutils/dialogs/basic.py
@@ -1,23 +1,19 @@
 """
 Contains convenient shortcuts to JOptionPane methods that show different kinds
-of dialogs. You can override the default error/warning dialog titles if your
-application's language is not English.
+of dialogs.
 
 """
 from javax.swing import JOptionPane
 
+__all__ = ('showErrorDialog', 'showWarningDialog', 'showMessageDialog')
 
-defaultErrorTitle = u'Error'
-defaultWarningTitle = u'Warning'
 
-def showErrorDialog(error, title=None, parent=None):
-    title = title if title is not None else defaultErrorTitle
+def showErrorDialog(error, title=u'Error', parent=None):
     JOptionPane.showMessageDialog(parent, error, title,
                                   JOptionPane.ERROR_MESSAGE)
 
 
-def showWarningDialog(message, title=None, parent=None):
-    title = title if title is not None else defaultWarningTitle
+def showWarningDialog(message, title=u'Warning', parent=None):
     JOptionPane.showMessageDialog(parent, message, title,
                                   JOptionPane.WARNING_MESSAGE)
 
